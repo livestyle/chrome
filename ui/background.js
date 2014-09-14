7924c9150518ef2a2f606f5d95e5575516144ab1
Sergey Chikuyonok <serge.che@gmail.com>
2014-09-15 01:07:30 +0400

    Working on UI

---
+++ ui/background.js
@@ -0,0 +1,5 @@
+define(function(require) {
+	chrome.browserAction.onClicked.addListener(function(tab) {
+		
+	});
+});
---
+++ ui/popup.js
@@ -0,0 +1,6 @@
+/**
+ * Main popup controller
+ */
+define(['./js/select-box'], function(selectBox) {
+	selectBox(document.body);
+})
