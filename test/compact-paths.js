e027a3aee3ce732d76c24cc3bf1e8e82a51dfa6f
Sergey Chikuyonok <serge.che@gmail.com>
2016-02-24 22:34:37 +0300

    Fixed unit test run for Travis

--- test/compact-paths.js
+++ test/compact-paths.js
@@ -1,6 +1,6 @@
 var assert = require('assert');
-require('babel/register');
-var compactPaths = require('../scripts/helpers/compact-paths');
+require('babel-register');
+var compactPaths = require('../src/lib/compact-paths').default;
 
 describe('Compact paths', function() {
 	function pluck(items, key) {
@@ -15,28 +15,28 @@ describe('Compact paths', function() {
 
 	it('keep names', function() {
 		assert.deepEqual(
-			process(['/path/to/file1.css', 'path/to/file2.css', 'file3.css']), 
+			process(['/path/to/file1.css', 'path/to/file2.css', 'file3.css']),
 			['file1.css', 'file2.css', 'file3.css']
 		);
 	});
 
 	it('keep partial names', function() {
 		assert.deepEqual(
-			process(['/path/to1/file.css', 'path/to2/file.css', 'file3.css']), 
+			process(['/path/to1/file.css', 'path/to2/file.css', 'file3.css']),
 			['to1/file.css', 'to2/file.css', 'file3.css']
 		);
 	});
 
 	it('keep full names', function() {
 		assert.deepEqual(
-			process(['/path1/to/file.css', 'path2/to/file.css', 'file3.css']), 
+			process(['/path1/to/file.css', 'path2/to/file.css', 'file3.css']),
 			['/path1/to/file.css', 'path2/to/file.css', 'file3.css']
 		);
 	});
 
 	it('Windows path separator', function() {
 		assert.deepEqual(
-			process(['C:\\path\\to\\file1.css', 'path/to/file2.css', 'file3.css']), 
+			process(['C:\\path\\to\\file1.css', 'path/to/file2.css', 'file3.css']),
 			['file1.css', 'file2.css', 'file3.css']
 		);
 	});
